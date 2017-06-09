6280c1f087708b665c8712c6d7fa8872159f8ed8
Connor80 <connor@connortroy.com>
2017-06-09 15:07:05 -0500

    implement multi-city feature

--- run.py
+++ run.py
@@ -12,36 +12,48 @@ def WeatherText():
     body = REQUEST.values.get('Body', None)
     resp = MessagingResponse()
 
-    if body.lower() == 'atlanta':
-        resp.message(atl_Welcome)
-        if body.lower() == 'sunset':
-            resp.message(atl_sunsetText)
-        elif body.lower() == 'moon':
-            resp.message(atl_moonText)
-        elif body.lower() == 'humidity':
-            resp.message(atl_humidityText)
-        elif body.lower() == 'wind':
-            resp.message(atl_windText)
-        elif body.lower() == 'rain':
-            resp.message(atl_rainText)
-        elif body.lower() == 'all':
-            resp.message(atl_allText)
-    elif body.lower() == 'dallas':
-        resp.message(dal_Welcome)
-        if body.lower() == 'sunset':
-            resp.message(dal_sunsetText)
-        elif body.lower() == 'moon':
-            resp.message(dal_moonText)
-        elif body.lower() == 'humidity':
-            resp.message(dal_humidityText)
-        elif body.lower() == 'wind':
-            resp.message(dal_windText)
-        elif body.lower() == 'rain':
-            resp.message(dal_rainText)
-        elif body.lower() == 'all':
-            resp.message(dal_allText)
-    else:
+    y = False
+    z = False
+    while not y and not z:
         resp.message(initial_Welcome)
+        if r.lower() == 'atlanta':
+            y = True   
+        elif r.lower() == 'dallas':
+            z = True        
+
+        while y is True:
+            resp.message(atl_Welcome)
+            if body.lower() == 'sunset':
+                resp.message(atl_sunsetText)
+            elif body.lower() == 'moon':
+                resp.message(atl_moonText)
+            elif body.lower() == 'humidity':
+                resp.message(atl_humidityText)
+            elif body.lower() == 'wind':
+                resp.message(atl_windText)
+            elif body.lower() == 'rain':
+                resp.message(atl_rainText)
+            elif body.lower() == 'all':
+                resp.message(atl_allText)
+            elif body.lower() == 'back':
+                y = False
+
+         while z is True:
+             resp.message(dal_Welcome)
+            if body.lower() == 'sunset':
+                resp.message(dal_sunsetText)
+            elif body.lower() == 'moon':
+                resp.message(dal_moonText)
+            elif body.lower() == 'humidity':
+                resp.message(dal_humidityText)
+            elif body.lower() == 'wind':
+                resp.message(dal_windText)
+            elif body.lower() == 'rain':
+                resp.message(dal_rainText)
+            elif body.lower() == 'all':
+                resp.message(dal_allText)
+            elif body.lower() == 'back':
+                z = False
     
     return str(resp)
 
